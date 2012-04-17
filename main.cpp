54d19241c5df56b759b6dd2f3c707e390df005a9
Brandt Westing <bmwesting@gmail.com>
2012-04-17 17:10:27 -0500

    Created sub-image for hand for efficient processing.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,11 @@ const unsigned int YRES = 480;
 
 const float DEPTH_SCALE_FACTOR = 255./4096.;
 
+// defines the value about which thresholding occurs
 const unsigned int BIN_THRESH_OFFSET = 5;
+
+// defines the value about witch the region of interest is extracted
+const unsigned int ROI_OFFSET = 70;
 
 int main(int argc, char** argv)
 {
@@ -24,11 +28,19 @@ int main(int argc, char** argv)
     // initialize the kinect
     sensor = new SkeletonSensor();
     sensor->initialize();
+    sensor->setPointModeToProjective();
 
     Mat depthRaw(YRES, XRES, CV_16UC1);
     Mat depthShow(YRES, XRES, CV_8UC1);
+    //Mat rightHand(ROI_OFFSET*2, ROI_OFFSET*2, CV_8UC1); // hand processing
+    
+    // rectangle used to extract hand regions from depth map
+    Rect roi;
+    roi.width  = ROI_OFFSET*2;
+    roi.height = ROI_OFFSET*2;
 
-    namedWindow("debugFrame", CV_WINDOW_AUTOSIZE);
+    namedWindow("depthFrame", CV_WINDOW_AUTOSIZE);
+    namedWindow("handFrame", CV_WINDOW_AUTOSIZE);
 
     int key = 0;
     while(key != 27 && key != 'q')
@@ -44,6 +56,8 @@ int main(int argc, char** argv)
             if(rightHand.confidence == 1.0)
             {
                 handDepth = rightHand.z * (DEPTH_SCALE_FACTOR);
+                roi.x = rightHand.x - ROI_OFFSET;
+                roi.y = rightHand.y - ROI_OFFSET;
                 printf("Hand depth = %d\n", handDepth);
             }
         }
@@ -54,11 +68,17 @@ int main(int argc, char** argv)
         memcpy(depthRaw.data, sensor->getDepthData(), XRES*YRES*2);
         depthRaw.convertTo(depthShow, CV_8U, DEPTH_SCALE_FACTOR);
 
-        //static binary threshold
+        // extract hand from image
+        Mat rightHand(depthShow, roi);
+         
+        // binary threshold
         if(handDepth != -1)
-            depthShow = (depthShow > (handDepth - BIN_THRESH_OFFSET)) & (depthShow < (handDepth + BIN_THRESH_OFFSET));
+            rightHand = (rightHand > (handDepth - BIN_THRESH_OFFSET)) & (rightHand < (handDepth + BIN_THRESH_OFFSET));
 
-        imshow("debugFrame", depthShow);
+       
+
+        imshow("depthFrame", depthShow);
+        imshow("handFrame",  rightHand);
 
         key = waitKey(10);
 
